e9cb450ddd27cb6ff6a186cc80f15b81285579b6
emmettzhang <emmettzhang@tencent.com>
2025-12-17 11:58:26 +0800

    HANG_DETECTION_ENABLED first init from env

--- src/monitor/enabler.rs
+++ src/monitor/enabler.rs
@@ -1,7 +1,15 @@
 use super::filter::Filter;
 use crate::monitor::LaunchCUDAKernel;
+use once_cell::sync::Lazy;
 use std::sync::atomic::{AtomicBool, Ordering};
-static HANG_DETECTION_ENABLED: AtomicBool = AtomicBool::new(false);
+
+static HANG_DETECTION_ENABLED: Lazy<AtomicBool> = Lazy::new(|| {
+    let enabled = std::env::var("HANG_DETECTION_ENABLED")
+        .map(|v| v == "1")
+        .unwrap_or(false);
+    log::info!("HANG_DETECTION_ENABLED [{}]", enabled);
+    AtomicBool::new(enabled)
+});
 
 pub struct Enabler {}
 
